c2a3c587ba1fb3305e84ae2b382e47890e51fa9d
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-09-30 15:09:06 +0530

    Implemented a transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,10 +5,21 @@ contract Token {
   string public symbol = "COIN";
   string public standard = "GLOBAL COIN V1.0";
   uint256 public totalSupply;
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+    );
   mapping (address => uint256) public balanceOf;
   constructor (uint256 _initialSupply) public  {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
-
+  }
+  function transfer(address _to, uint256 _value) public returns(bool success){
+    require(balanceOf[msg.sender]  >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- test/Token.js
+++ test/Token.js
@@ -29,4 +29,29 @@ contract('Token', function(accounts){
     assert.equal(adminBalance.toNumber(), 1000000, 'allocates the initial supply to the admin ')
   })
     })
+
+    it('transfers tokens', function(){
+      return Token.deployed().then(function(instance){
+        tokenInstance = instance;
+        return tokenInstance.transfer.call(accounts[1], 9999999999999999);
+      }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+        return tokenInstance.transfer.call(accounts[1],250000, {from: accounts[0]})
+      }).then(function(success){
+        assert.equal(success, true, 'transfer returns true');
+        return tokenInstance.transfer(accounts[1],250000, {from: accounts[0]})
+      }).then(function(receipt){
+        assert.equal(receipt.logs.length, 1, 'triggers one event');
+        assert.equal(receipt.logs[0].event, 'Transfer', 'the event is "Transfer" event');
+        assert.equal(receipt.logs[0].args._from, accounts[0], 'the account that sends token');
+        assert.equal(receipt.logs[0].args._to, accounts[1], 'the account that receives the token');
+        assert.equal(receipt.logs[0].args._value, 250000, 'the transfer amount');
+        return tokenInstance.balanceOf(accounts[1]);
+      }).then(function(balance){
+        assert.equal(balance.toNumber(),250000,'amount is added to the transferred account');
+        return tokenInstance.balanceOf(accounts[0]);
+      }).then(function(balance){
+        assert.equal(balance.toNumber(),750000,'amount is deducted from the withdrawn account')
+      })
+    })
 })
